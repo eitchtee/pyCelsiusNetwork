0e1d6d1b10a4e4f340bbb07f50cdeeb9b9816ac6
eitchtee <flaips.2@gmail.com>
2020-08-08 23:28:34 -0300

    Add get_transactions() to wrapper

--- pycelsiusnetwork/celsius.py
+++ pycelsiusnetwork/celsius.py
@@ -45,3 +45,40 @@ class CelsiusNetwork:
         else:
             return response.json().get('amount'),\
                    response.json().get('amount_in_usd'),
+
+    def get_transactions(self, raw: bool = False, **kwargs):
+        page = kwargs.get('page') or 1
+        per_page = kwargs.get('per_page') or 100
+        url = f"https://wallet-api.celsius.network/" \
+              f"wallet/transactions?page={page}&per_page={per_page}"
+
+        headers = {
+            'X-Cel-Partner-Token': self.token,
+            'X-Cel-Api-Key': self.key}
+
+        response = requests.request("GET", url, headers=headers)
+
+        if not response.ok:
+            raise CelsiusNetworkHTTPError(response)
+
+        if raw:
+            return response.json()
+        else:
+            pagination = response.json().get('pagination')
+
+            result = []
+            result += response.json().get('record')
+
+            if pagination['pages'] > page:
+                for next_page in range(
+                        pagination['current'] + 1, pagination['pages'] + 1):
+
+                    url = f"https://wallet-api.celsius.network" \
+                          f"/wallet" \
+                          f"/transactions?page={next_page}&per_page={per_page}"
+
+                    response = requests.request("GET", url, headers=headers)
+
+                    result += response.json().get('record')
+
+            return result
